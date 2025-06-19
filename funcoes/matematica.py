abb254db21db9dfdcac2f0379d35c24341a8d600
Andrea Thesolin <andreathe@yahoo.com.br>
2025-06-19 14:52:52 -0300

    Criando primeiro pipeline 2

---
+++ funcoes/matematica.py
@@ -0,0 +1,10 @@
+
+def soma(a, b):
+    return a + b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Não pode dividir por zero!")
+    return a / b
+
+# Crie outras funções
---
+++ tests/test_matematica.py
@@ -0,0 +1,16 @@
+rom funcoes import matematica
+
+def test_soma():
+    assert matematica.soma(2, 3) == 5
+
+def test_divide():
+    assert matematica.divide(10, 2) == 5
+
+def test_divide_zero():
+    try:
+        matematica.divide(10, 0)
+        assert False
+    except ValueError:
+        assert True
+
+# Crie testes para todos os casos das outras funções que você criou
